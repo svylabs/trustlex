5b9410e5808a46c4efe04c7fc86dd42c5ca74472
svylabs <sg@svylabs.com>
2023-01-23 00:57:56 +0100

    Partial implementation of initiate fulfillment request

--- contracts/TrustlexPerAssetOrderBook.sol
+++ contracts/TrustlexPerAssetOrderBook.sol
@@ -9,25 +9,30 @@ contract TrustlexPerAssetOrderBook {
 
     struct FulfillmentRequest {
         address fulfillmentBy;
-        uint256 quantityRequested;
+        uint64 quantityRequested;
         bool allowAnyoneToSubmitPaymentProofForFee;
+        bool allowAnyoneToAddCollateralForFee;
         uint256 totalCollateralAdded;
-        uint32 fee;
+        uint32 expiryTime;
         uint32 fulfilledTime;
+        address collateralAddedBy;
     }
 
     struct Offer {
-        address orderedBy;
-        uint256 orderQuantity;
+        address offeredBy;
+        uint256 offerQuantity;
         uint64 satoshisToReceive;
         uint64 satoshisReceived;
+        uint64 satoshisReserved;
         bytes20 bitcoinAddress;
         uint32 offerValidTill;
-        uint8 minimumCollateral;
+        uint8 collateralPer3Hours;
         uint32 orderedTime;
+        uint32 offeredBlockNumber;
+        uint256[] fulfillmentRequests;
     }
 
-    mapping (uint256 => mapping (address => FulfillmentRequest)) initializedOrders;
+    mapping (uint256 => mapping(uint256 => FulfillmentRequest)) initializedFulfillments;
 
     mapping (uint256 => Offer) public offers;
 
@@ -44,31 +49,65 @@ contract TrustlexPerAssetOrderBook {
     function addOfferWithEth(uint64 satoshis, bytes20 bitcoinAddress, uint32 offerValidTill) public payable {
         require(tokenContract == address(0x0));
         Offer memory offer = _offer;
-        offer.orderedBy = msg.sender;
-        offer.orderQuantity = msg.value;
+        offer.offeredBy = msg.sender;
+        offer.offerQuantity = msg.value;
         offer.satoshisToReceive = satoshis;
         offer.bitcoinAddress = bitcoinAddress;
         offer.offerValidTill = offerValidTill;
-        uint256 offerId = uint256(keccak256(abi.encode(offer, block.number)));
+        offer.offeredBlockNumber = uint32(block.number);
+        uint256 offerId = uint256(keccak256(abi.encode(offer)));
         offers[offerId] = offer;
         emit NEW_OFFER(offerId);
     }
 
-    function addOfferWithToken(uint256 value, uint64 satoshis, bytes20 bitcoinAddress, uint32 offerValidTill)) public {
+    function addOfferWithToken(uint256 value, uint64 satoshis, bytes20 bitcoinAddress, uint32 offerValidTill) public {
         require(tokenContract != address(0x0));
         Offer memory offer = _offer;
-        offer.orderedBy = msg.sender;
-        offer.orderQuantity = value;
+        offer.offeredBy = msg.sender;
+        offer.offerQuantity = value;
         offer.satoshisToReceive = satoshis;
         offer.bitcoinAddress = bitcoinAddress;
         offer.offerValidTill = offerValidTill;
-        uint256 offerId = uint256(keccak256(abi.encode(offer, block.number)));
+        offer.offeredBlockNumber = uint32(block.number);
+        uint256 offerId = uint256(keccak256(abi.encode(offer)));
         offers[offerId] = offer;
         emit NEW_OFFER(offerId);
     }
 
-    function initiateFulfillment(uint256 offerId, FulfillmentRequest calldata fulfillment) public {
-        
+    function initiateFulfillment(uint256 offerId, FulfillmentRequest calldata _fulfillment) public payable {
+        uint64 satoshisToReceive = offers[offerId].satoshisToReceive;
+        uint64 satoshisReserved = offers[offerId].satoshisReserved;
+        uint64 satoshisReceived = offers[offerId].satoshisReceived;
+        uint64 quantityRequested = _fulfillment.quantityRequested;
+        if (satoshisToReceive == (satoshisReserved + satoshisReceived)) {
+            uint256[] memory fulfillmentIds = offers[offerId].fulfillmentRequests;
+            for (uint256 index = 0; index < fulfillmentIds.length;index++) {
+                FulfillmentRequest memory existingFulfillmentRequest = initializedFulfillments[offerId][fulfillmentIds[index]];
+                if (existingFulfillmentRequest.expiryTime < block.timestamp) {
+                    require(quantityRequested == existingFulfillmentRequest.quantityRequested);
+                    // TODO: Claim any satoshis reserved
+                    offers[offerId].satoshisReserved -= existingFulfillmentRequest.quantityRequested;
+                    // TODO: Claim collateral
+
+                }
+            }
+            satoshisReserved = offers[offerId].satoshisReserved;
+        }
+        require(satoshisToReceive >= (satoshisReserved + satoshisReceived + _fulfillment.quantityRequested));
+        FulfillmentRequest memory fulfillment = _fulfillment;
+        fulfillment.fulfillmentBy = msg.sender;
+        if (satoshisReserved > 0) {
+            require(fulfillment.totalCollateralAdded > offers[offerId].collateralPer3Hours);
+        }
+        if (fulfillment.totalCollateralAdded > 0 && tokenContract == address(0x0)) {
+            fulfillment.totalCollateralAdded = msg.value;
+        } else if (fulfillment.totalCollateralAdded > 0) {
+            // TODO: Get tokens from tokenContract
+            fulfillment.collateralAddedBy = msg.sender;
+        }
+        fulfillment.expiryTime  = uint32(block.timestamp);
+        uint256 fulfillmentId = uint256(keccak256(abi.encode(fulfillment, block.timestamp)));
+        initializedFulfillments[offerId][fulfillmentId] = fulfillment;
     }
 
     /*
@@ -85,7 +124,11 @@ contract TrustlexPerAssetOrderBook {
 
     }
 
-    function addCollateral() public payable {
+    function addEthCollateral() public payable {
+
+    }
+
+    function addTokenCollateral() public payable {
 
     }
 
