3d53f2672e7c7d7b01a8c98395812d6ba3dae5c6
svylabs <sg@svylabs.com>
2023-02-01 21:53:20 +0100

    Adding some data

--- test/TrustlexOrderBookTest.js
+++ test/TrustlexOrderBookTest.js
@@ -1,16 +1,26 @@
-var BitcoinUtils = artifacts.require("BitcoinUtils");
-var SafeMath = artifacts.require('SafeMath');
+//var BitcoinUtils = artifacts.require("BitcoinUtils");
+//var SafeMath = artifacts.require('SafeMath');
 const trustlexapp = artifacts.require("TrustlexPerAssetOrderBook");
-const fs = require('fs');
 
 contract("Create TrustlexOrderBookContract", (accounts) => {
     const [owner, user1, user2] = accounts;
-    const txParams = { from: owner };
-    it ('submit block', async function() {
-        trustlexapp.link(await BitcoinUtils.deployed());
-        trustlexapp.link(await SafeMath.deployed());
-        this.trustlexappcontract = await trustlexapp.new('0x0', txParams);
+    const txParam = { from: owner};
+    const txParams = { from: owner, value: web3.utils.toWei('10',"ether") };
+    const txParams1 = { from: user1, value: web3.utils.toWei('8.5',"ether") };
+    const txParams2 = { from: user2, value: web3.utils.toWei('7',"ether") };
+    it ('Create contract and add some data', async function() {
+        //trustlexapp.link(await BitcoinUtils.deployed());
+        //trustlexapp.link(await SafeMath.deployed());
+        this.trustlexappcontract = await trustlexapp.new('0x0', txParam);
         console.log(this.trustlexappcontract.address);
+        //function addOfferWithEth(uint64 satoshis, bytes20 bitcoinAddress, uint32 offerValidTill) public payable {
+        await this.trustlexappcontract.addOfferWithEth(100000000, 
+            '0x0000000000000000000000000000000000000000', 
+            (parseInt(new Date().getTime() / 1000) + 7 * 24 * 60 * 60),
+            txParams
+            );
+        await this.trustlexappcontract.addOfferWithEth(90000000, '0x0000000000000000000000000000000000000001', (parseInt((new Date).getTime() / 1000) + 6 * 24 * 60 * 60), txParams1);
+        await this.trustlexappcontract.addOfferWithEth(80000000, '0x0000000000000000000000000000000000000003', (parseInt((new Date).getTime() / 1000) + 6 * 24 * 60 * 60), txParams2);
     });
 })
 
