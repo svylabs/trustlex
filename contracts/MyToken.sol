d003f9805f93bf68f0a9377bfae7317171cdc697
Glen <Glen.n@appwrk.com>
2023-04-27 16:23:33 +0530

    Added the ERC20 token new contract

---
+++ contracts/MyToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.8.3/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.8.3/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.8.3/access/Ownable.sol";
+
+contract MyToken is ERC20, ERC20Burnable, Ownable {
+    constructor(
+        uint256 initialBalance,
+        address creditToaddress
+    ) ERC20("BitcoinSPVCoin", "SPVC") {
+        _mint(creditToaddress, initialBalance);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
